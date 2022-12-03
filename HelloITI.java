eee99c7318b371316d312668d1ed3e113cf0451a
mohamed-ie <mohammedie98@gmail.com>
2022-12-03 12:16:13 +0200

    file-modified HelloJava.java

--- HelloITI.java
+++ HelloITI.java
@@ -3,6 +3,7 @@ public class HelloITI{
 	public static void main (String [] args){
 		printHelloITI();
 		printHelloGit();
+		printHelloGitLab2();
 	}
 
 	static void printHelloITI(){
@@ -11,5 +12,9 @@ public class HelloITI{
 
 	static void printHelloGit(){
 		System.out.println("Hello Git!");
+	}
+
+	static void printHelloGitLab2(){
+	System.out.println("Hello Git lab2!");
 	}
 }
